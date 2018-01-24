6dd0c720b6a50446fabb8adbd4b519d564c25f95
Lance Moreland <lmoreland1899@gmail.com>
2018-01-24 15:20:20 -0500

    Project Complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Math.pow(3,5) = " + Math.pow(3,5));
+        System.out.println( 10.0 / 7);
+        System.out.println("5 * Math.pow(2,5) - 4 = " + (5 * Math.pow(2,5) - 4));
+        System.out.println("10.3 % 5 = " + 10.3 % 5);
+        System.out.println(5536253 % 6);
+
+        System.out.println("Math.random() Examples: ");
+        System.out.println("Math.random() * 10 = " + Math.random() * 10 );
+        System.out.println("Math.random() * 60.1 = " + Math.random() * 60.1 );
+
+        System.out.println("Math.ceil() examples: ");
+        System.out.println("Math.ceil(Math.random() * 10 = " + Math.ceil(Math.random() * 10));
+
+        System.out.println("Math.floor() examples: ");
+        System.out.println("Math.floor(10.3 * 5) = " + Math.floor(10.3*5));
+
+        System.out.println("Math.round(4.5) = " + Math.round(4.5));
+        System.out.println("Math.round(4.4) = " + Math.round(4.4));
+        System.out.println("Math.round(Math.random() * 10) = " + (Math.random() * 10));
+
     }
 }
